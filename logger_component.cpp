c7d5ead691015e82f4f2c10017c205bb95122b38
cuppajoeman <ccn@cuppajoeman.com>
2024-09-30 04:25:19 -0400

    feat!: take in list of loggers

--- logger_component.cpp
+++ logger_component.cpp
@@ -1,21 +1,25 @@
 #include "logger_component.hpp"
 
-LoggerComponent::LoggerComponent(const std::string &logger_name, std::shared_ptr<spdlog::sink_ptr> shared_sink) {
-    logger = std::make_shared<spdlog::logger>(logger_name, *shared_sink);
-    logger->set_level(spdlog::level::debug);
-    spdlog::register_logger(logger);
-    logging_enabled = true;
+LoggerComponent::LoggerComponent(const std::string &logger_name, const std::vector<spdlog::sink_ptr> &sinks) {
+    if (!sinks.empty()) {
+        // Create a logger with multiple sinks
+        logger = std::make_shared<spdlog::logger>(logger_name, sinks.begin(), sinks.end());
+        logger->set_level(spdlog::level::debug);
+        spdlog::register_logger(logger);
+        logging_enabled = true;
+    } else {
+        logger = nullptr;
+        logging_enabled = false;
+    }
 }
 
-LoggerComponent::LoggerComponent() {
-    logger = nullptr;
-    logging_enabled = false;
-}
+LoggerComponent::LoggerComponent() : logger(nullptr), logging_enabled(false) {}
 
 void LoggerComponent::set_logging(bool enable, const std::string &logger_name,
-                                  std::shared_ptr<spdlog::sink_ptr> shared_sink) {
-    if (enable && !logger && shared_sink) {
-        logger = std::make_shared<spdlog::logger>(logger_name, *shared_sink);
+                                  const std::vector<spdlog::sink_ptr> &sinks) {
+    if (enable && !logger && !sinks.empty()) {
+        // Create the logger with multiple sinks
+        logger = std::make_shared<spdlog::logger>(logger_name, sinks.begin(), sinks.end());
         logger->set_level(spdlog::level::debug);
         spdlog::register_logger(logger);
     }
--- logger_component.hpp
+++ logger_component.hpp
@@ -1,20 +1,22 @@
 #ifndef LOGGER_COMPONENT_HPP
 #define LOGGER_COMPONENT_HPP
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <memory>
 #include <string>
+#include <vector>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 class LoggerComponent {
   public:
-    LoggerComponent(const std::string &logger_name, std::shared_ptr<spdlog::sink_ptr> shared_sink);
+    LoggerComponent(const std::string &logger_name, const std::vector<spdlog::sink_ptr> &sinks);
     LoggerComponent();
-    void set_logging(bool enable, const std::string &logger_name = "",
-                     std::shared_ptr<spdlog::sink_ptr> shared_sink = nullptr);
+
+    void set_logging(bool enable, const std::string &logger_name, const std::vector<spdlog::sink_ptr> &sinks);
 
     std::shared_ptr<spdlog::logger> get_logger() const;
-    bool logging_enabled; /// true implies logger != null
+    bool logging_enabled;
 
   private:
     std::shared_ptr<spdlog::logger> logger;
